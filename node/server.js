dd0cee83970cec67c4b2762a001795ad9b2f6e3e
José Manuel Sánchez <buscarini@gmail.com>
2014-05-09 08:48:22 +0200

    Now observes file changes in csv and imports it automatically. Added gitignore

--- node/server.js
+++ node/server.js
@@ -4,12 +4,15 @@ var mongoose = require('mongoose');
 var express = require('express');
 var importer = require('./BookImporter')
 var Book_list = require('./Book_list')
+var fs = require('fs')
 	
 var app = express();
 
+var csvFile = __dirname+"/books.csv";
+
 app.get('/import', function(req, res) {
 	
-	importer.importFile(__dirname+"/books.csv",function(err,books) {
+	importer.importFile(csvFile,function(err,books) {
 
 		var response = ''
 		
@@ -23,6 +26,17 @@ app.get('/import', function(req, res) {
 	})
 })
 
+fs.watchFile(csvFile, function(curr,prev) {
+	console.log("watching file")
+	if (prev.mtime.getTime()!=curr.mtime.getTime()) {
+		console.log("importing csv")
+		importer.importFile(csvFile,function(err,books) {
+			if (err) {
+				console.log(err);
+			}
+		});		
+	}
+});
 
 app.get('/books', function(req, res) {
 	Book_list.findAll(req,res,function() {
@@ -35,28 +49,3 @@ var server = app.listen(3000, function() {
 		
     console.log('Listening on port %d', server.address().port);
 });
-
-// 
-// 
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function callback () {
-// 	var http = require('http');
-// 	http.createServer(function (req, res) {
-// 		
-// 		importer.importFile(__dirname+"/books.csv",function(err,books) {
-// 			res.writeHead(200, {'Content-Type': 'text/plain'});
-// 		
-// 			if (!err) {
-// 				books.forEach(function(book) {
-// 					res.write(book.id + " " + book.title + "\n")
-// 				});
-// 			}
-// 		
-// 			res.end("");
-// 		})
-// 
-// 	}).listen(8124, "127.0.0.1");
-// 	console.log('Server running at http://127.0.0.1:8124/');
-// });
-
